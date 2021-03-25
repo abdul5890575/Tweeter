260312d4d1b1630df105abf425759079ad926a1d
Abdul rehman Shaukat <abdulrehman.shoukat@email.com>
2021-03-25 01:07:55 +0000

    day3

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,83 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+$(document).ready(() => {
+  // $('#errorlong').hide()
+  // $('#errornull').hide()
+  loadTweets()
+  $('form').on('submit', handleSubmit)
+
+})
+
+const createTweetElement = (tweetObj) => {
+    const tweet = `<article class="articlehead">
+                         <header class="tweetheader">
+                        <img src="${tweetObj['user']['avatars']}">
+                        <h3>${tweetObj['user']['name']}</h3>
+                        <div class="rightname">${tweetObj['user']['handle']}</div>
+                      </header>
+                      <section class="articlebody">
+                        <p>${tweetObj['content']['text']}</p>
+                        <hr>
+                      </section>
+                      <footer class="articlefooter">
+                        <p>10 days ago</p>
+                        <div class="icons">
+                          <i class="fas fa-flag"></i>
+                          <i class="fas fa-retweet"></i>
+                          <i class="fas fa-heart"></i>
+                        </div>
+                      </footer>
+                      </article>`
+  
+    return tweet; 
+}
+
+
+const renderTweets = function(data) {
+  $('#tweet-container').empty();
+  for (let userobj of data) {
+    let $tweet = createTweetElement(userobj);
+    $('#tweet-container').prepend($tweet)
+  }
+}
+
+const handleSubmit = function (event)  {
+  event.preventDefault();
+  let data = $(this).serialize();
+  
+  if( $('#tweet-text').val() === '') {
+    $('#errornull').slideDown()
+  } else if ($('#tweet-text').val().length > 140) {
+    $('#errorlong').slideDown()
+  } else {
+    
+    $.ajax({
+      url: "/tweets",
+      method: "POST",
+      data: data
+    })
+      .then(res => {
+        console.log('sent to tweets', res)
+        loadTweets();
+      })
+      .catch(err => console.log(err))
+  }
+}
+
+const loadTweets = () =>{
+  $('#errorlong').hide()
+  $('#errornull').hide()
+  $.ajax({
+    url: "/tweets",
+    method: "GET"
+  })
+    .then(res => {
+      renderTweets(res)
+    })
+    .catch(err => console.log(err))
+}
+
+
+
+
